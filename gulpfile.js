8d4263bf11f1e195d1814553c60e98dd7478234d
Alexander Ferguson <x@echonotation.com>
2017-10-23 11:27:32 -0400

    Add minification and babel to support es6 uglification

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
 var gulp = require('gulp'),
     stylus = require('gulp-stylus'),
+    cleanCSS = require('gulp-clean-css'),
     autoprefixer = require('autoprefixer-stylus'),
-    jshint = require('gulp-jshint');
+    jshint = require('gulp-jshint'),
+    babel = require('gulp-babel'),
+    uglify = require('gulp-uglify');
 
 
 gulp.task('stylus', function() {
-    gulp.src('css/*.styl')
+    gulp.src('./css/*.styl')
     .pipe(stylus({
         use: [autoprefixer()]
     }))
-    .pipe(gulp.dest('css/'));
+    .pipe(gulp.dest('./css/'));
 });
+
+gulp.task('css_min', ['stylus'], function() {
+    gulp.src('./css/default.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./build/'))
+})
 
 gulp.task('lint', function() {
 	return gulp.src('./js/prosody.js')
@@ -25,10 +34,21 @@ gulp.task('lint', function() {
 	.pipe(jshint.reporter('default'))
 });
 
+gulp.task('js_uglify', ['lint'], function() {
+    return gulp.src('./js/prosody.js')
+    .pipe(babel({
+      presets: ['env']
+    }))
+    .pipe(uglify().on('error', function(e){
+        console.log(e);
+     }))
+    .pipe(gulp.dest('./build/'))
+});
+
 gulp.task('watch', function() {
     gulp.watch('**/*.styl', ['stylus']);
 });
 
-gulp.task('default', ['stylus', 'watch']);
+gulp.task('build', ['stylus', 'css_min', 'js_uglify']);
 
-gulp.task('build', ['stylus', 'lint']);
+gulp.task('default', ['build', 'watch']);
